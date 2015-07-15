fb1de3b970aed041268dce8b68627313bf10ef8c
Jenny Crimp <jenny.crimp@gmail.com>
2015-07-14 17:46:05 -0700

    Adds News Create controller, and ability to add news data

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -10,6 +10,6 @@
         <div class="main">
                 <?php echo $news_item['text'] ?>
         </div>
-        <p><a href="<?php echo $news_item['slug'] ?>">View article</a></p>
+        <p><a href="news/<?php echo $news_item['slug'] ?>">View article</a></p>
 
 <?php endforeach ?>
---
+++ application/views/news/success.php
@@ -0,0 +1,6 @@
+<?php
+//application/views/news/feedback.php
+?>
+<h2><?php echo $title ?></h2>
+
+<p>Success!</p>
